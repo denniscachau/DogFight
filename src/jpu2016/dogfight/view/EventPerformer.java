bd17c1ad62f2058a077ab78e4549eb10fed88135
Romainlemagnifique <romain7@sfr.fr>
2018-04-24 10:11:48 +0200

    phase3encours

---
+++ src/jpu2016/dogfight/view/EventPerformer.java
@@ -0,0 +1,9 @@
+package jpu2016.dogfight.view;
+
+public class EventPerformer {
+
+	public EventPerformer (IOrderPerformer orderPerformer){}
+	public void EventPerform (KeyEvent keyCode) {}
+	private UserOrder CodetoUserOrder (int KeyCode) {}
+	
+}
