3e43ff08d607b80cb36644474b5a33da1a9851aa
Romainlemagnifique <romain7@sfr.fr>
2018-04-24 11:11:08 +0200

    phase 3 en cours

---
+++ src/jpu2016/gameframe/GamePanel.java
@@ -0,0 +1,7 @@
+package jpu2016.gameframe;
+
+public class GamePanel {
+	public GamePanel(IGraphicsBuilder graphicBuilder) {} 
+	public update (Observable observable) {}
+	public void paintComponent (Graphics graphic) {}
+}
