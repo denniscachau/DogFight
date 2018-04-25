291d2d852ea13825e81d3a15c02ca072a6a53dc4
Vladou <vladimir.camalot@orange.fr>
2018-04-25 06:38:12 +0200

    phase 3

---
+++ src/jpu2016/dogfight/model/DogfightModel.java
@@ -0,0 +1,22 @@
+package jpu2016.dogfight.model;
+
+import java.util.ArrayList;
+
+public class DogfightModel implements IDogfightModel{
+	public DogfightModel() {	
+	}
+	public IArea getArea() {	
+	}
+	public void buildArea(Dimension dimension) {	
+	}
+	public void addMobile(IMobile Mobile) {	
+	}
+	public void removeMobile(IMobile Mobile) {	
+	}
+	public ArrayList<IMobile> getMobiles();	
+	
+	public IMobile getMobileByPlayer(int player) {	
+	}
+	public void setMobileHavesMoved() {	
+	}
+}
