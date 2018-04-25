246cc85a9780257e98ab687b57622d82a0466935
Vladou <vladimir.camalot@orange.fr>
2018-04-25 06:59:10 +0200

    phase 3

--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -1,4 +1,8 @@
 package jpu2016.dogfight.model;
+
+import java.awt.Image;
+import java.awt.Point;
+
 public class Mobile implements IMobile{
 	public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {	
 	}
@@ -9,5 +13,75 @@ public class Mobile implements IMobile{
 	private void moveDown() {	
 	}
 	private void moveLeft() {	
+	}
+	@Override
+	public Direction getDirection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setDirection(Direction direction) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public Point getPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public Dimension getDimension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public int getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void move() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void placeInArea(IArea area) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean isPlayer(int player) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public void setDogfightModel(DogfightModel dogfightModel) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean hit() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isWeapon() {
+		// TODO Auto-generated method stub
+		return false;
 	}
 }
--- src/jpu2016/dogfight/model/Sky.java
+++ src/jpu2016/dogfight/model/Sky.java
@@ -1,5 +1,19 @@
 package jpu2016.dogfight.model;
 
+import java.awt.Image;
+
 public class Sky implements IArea{
+
+	@Override
+	public Dimension getDimension() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 }
