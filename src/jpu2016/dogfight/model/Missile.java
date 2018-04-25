3f036555061f4a32fa892357bb3723125fd43409
Vladou <vladimir.camalot@orange.fr>
2018-04-25 06:53:05 +0200

    phase 3

--- src/jpu2016/dogfight/model/Missile.java
+++ src/jpu2016/dogfight/model/Missile.java
@@ -21,4 +21,4 @@ public void move() {
 }
 public boolean isWeapon() {
 }
-}
+  }
--- src/jpu2016/dogfight/model/Mobile.java
+++ src/jpu2016/dogfight/model/Mobile.java
@@ -1,1 +1,13 @@
 package jpu2016.dogfight.model;
+public class Mobile implements IMobile{
+	public Mobile(Direction direction, Position position, Dimension dimension, int speed, String image) {	
+	}
+	public void moveUp() {	
+	}
+	private void moveRight() {	
+	}
+	private void moveDown() {	
+	}
+	private void moveLeft() {	
+	}
+}
